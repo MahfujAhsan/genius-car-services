aee0dc112e7db08b3cb37475b506aa76a10cbaab
Mahfuj Ahsan <ahsanmahfuj@gmail.com>
2022-04-13 07:03:51 +0600

    service responsiveness updated

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
+import './Services.css';
 
 const Services = () => {
     const [services, setServices] = useState([]);
     useEffect(() => {
         fetch('services.json')
-        .then(res => res.json())
-        .then(data => setServices(data))
+            .then(res => res.json())
+            .then(data => setServices(data))
     }, [])
 
     return (
         <div>
-            <h2>Services: {services.length}</h2>
-            {
-                services.map(service => <Service key={service.id} service={service}></Service>)
-            }
+            <h1 className='services-title'>Our Services</h1>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service key={service.id} service={service}></Service>)
+                }
+            </div>
         </div>
     );
 };
