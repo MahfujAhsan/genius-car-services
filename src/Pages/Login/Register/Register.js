535bff92c28ee86b6f8ba711e2808537b29538f5
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-04-15 00:05:33 +0600

    google auth updated

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import "./Register.css";
 import auth from "../../../firebase.init"
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
     const [
@@ -29,16 +30,17 @@ const Register = () => {
     }
     return (
         <div className='register-form'>
-            <h2 className='text-primary text-center mt-2'>Please Register</h2>
+            <h2 className='text-primary text-center my-4'>Please Register</h2>
             <form onSubmit={handleRegister}>
-                <input type="text" name="name" id="" placeholder='Your Name'/>
+                <input  type="text" name="name" id="" placeholder='Your Name'/>
                 
                 <input type="email" name="email" id="" placeholder='Your Email' required/>
                 
                 <input type="password" name="password" id="" placeholder='Your Password' required/>
-                <input type="submit" value="Register" />
+                <input className='bg-dark text-white fw-bold text-uppercase w-50 mx-auto rounded my-4' type="submit" value="Register" />
             </form>
             <p>Already Have an Account? <Link to="/login" className='text-danger fw-bold pe-auto text-decoration-none' onClick={navigateLogin}>Please Login.</Link></p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
---
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import google from "../../../images/social/google.png"
+import facebook from "../../../images/social/facebook.png"
+import github from "../../../images/social/github.png"
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+import { useNavigate } from 'react-router-dom';
+
+const SocialLogin = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const navigate = useNavigate();
+    let errorElement;
+    if (error) {
+        errorElement = <div>
+            <p className='text-danger'>Error: {error.message}</p>
+        </div>
+
+    }
+    if (user) {
+        navigate("/home")
+    }
+    return (
+        <div>
+            <div className='d-flex align-items-center'>
+                <div style={{ height: '1px' }} className="bg-primary w-50"></div>
+                <p className='mt-2 px-2'>or</p>
+                <div style={{ height: '1px' }} className="bg-primary w-50"></div>
+            </div>
+            {errorElement}
+            <div className=''>
+                <button onClick={() => signInWithGoogle()} className='btn btn-light w-50 d-block mx-auto border border-2 border-dark'>
+                    <img style={{ width: "30px" }} src={google} alt="" />
+                    <span className='px-3'>GooGle Sign In</span>
+                </button>
+                <button className='btn btn-light my-3 w-50 d-block mx-auto border border-2 border-dark'>
+                    <img style={{ width: "30px" }} src={facebook} alt="" />
+                    <span className='px-3'>Facebook Sign In</span>
+                </button>
+                <button className='btn btn-light w-50 d-block mx-auto border border-2 border-dark'>
+                    <img style={{ width: "30px" }} src={github} alt="" />
+                    <span className='px-3'>GitHub Sign In</span>
+                </button>
+            </div>
+        </div>
+
+    );
+};
+
+export default SocialLogin;
