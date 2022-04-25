1f1ec49a84dad3d84ec6eb121f6707e30e862b17
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-04-25 10:39:04 +0600

    state correction update

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -3,15 +3,18 @@ import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
-    const [service, setService] = useState([]);
+    const [service, setService] = useState({});
     useEffect(() => {
-        
+        const url = `http://localhost:5000/service/${serviceId}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setService(data))
     }, [])
     return (
         <div>
-            <h2>Welcome To Detail {serviceId}</h2>
+            <h2>Welcome To Detail: {service.name}</h2>
             <div className='text-center'>
-                <Link to="/checkout">
+                <Link to={`/checkout/${serviceId}`}>
                     <button className='btn btn-primary'>Proceed Checkout</button>
                 </Link>
             </div>
